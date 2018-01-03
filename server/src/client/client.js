606a9b91f72f90e24ef5119db73da55d4031f94f
Shingo Tomioka <s.tomioka.103@gmail.com>
2018-01-03 17:22:31 +0900

    Use routes in client

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDom.hydrate(<Home />, document.getElementById('root'));
+ReactDom.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
