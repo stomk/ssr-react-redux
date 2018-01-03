ca61a9edb254f0320f2c80c8a96ea628c1052b21
Shingo Tomioka <s.tomioka.103@gmail.com>
2018-01-03 15:02:47 +0900

    Hydrate app on client

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,1 +1,5 @@
-console.log('Hi there!');
+import React from 'react';
+import ReactDom from 'react-dom';
+import Home from './components/Home';
+
+ReactDom.hydrate(<Home />, document.getElementById('root'));
