57e911269dce86984126893006ab2463d87a519e
Shingo Tomioka <s.tomioka.103@gmail.com>
2018-01-04 13:15:22 +0900

    Rewrite routes with react-router-config

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -5,7 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import Routes from './Routes';
+import { renderRoutes } from 'react-router-config';
+import routes from './routes';
 import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
@@ -13,7 +14,7 @@ const store = createStore(reducers, {}, applyMiddleware(thunk));
 ReactDom.hydrate(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes />
+      {renderRoutes(routes)}
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
