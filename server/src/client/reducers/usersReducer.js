aedfff091aef2ac1ba4b180b31d2fe498da8e23c
Shingo Tomioka <s.tomioka.103@gmail.com>
2018-01-04 11:35:41 +0900

    Fix typo

--- server/src/client/reducers/usersReducer.js
+++ server/src/client/reducers/usersReducer.js
@@ -1,8 +1,8 @@
 import { FETCH_USERS } from '../actions';
 
-export default (state = [], actions) => {
-  switch(action.type) {
-    case(FETCH_USERS):
+export default (state = [], action) => {
+  switch (action.type) {
+    case FETCH_USERS:
       return action.payload.data;
     default:
       return state;
